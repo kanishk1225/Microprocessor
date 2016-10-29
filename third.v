531e12e3aa2aaed5e1afd334b6d6c699b0f510c6
Kanishk Goyal <kanishk125.ucs2015@iitr.ac.in>
2016-10-29 19:18:31 +0530

    Add files via upload

--- third.v
+++ third.v
@@ -1,47 +1,58 @@
-`timescale 1ns / 1ps
+module alu_8mod(out,a,b,s);
 
+input [8:0]a,b;
 
-module third(Out,A,B,Sel);
-	output [7:0] Out;
-	input [7:0] A, B;
-	input [1:0] Sel;
-	reg [7:0] A_1, B_1, Out_1;
-	reg [1:0]Sel_1;
+input [3:0]s;
 
-	wire [6:0] Carry;
-	wire ovr;
-	
-	initial
-	begin
-		Sel_1 = 00;
-	end
-	
-	if (Sel_1=="00")
-	begin
-		if (A_1[0]=="0" && B_1[0]=="0")
-		begin
-			integer t = 0;
-			fa FA1(Out_1[7],Carry[6],A[7],B[7],t);
-			fa FA2(Out_1[6],Carry[5],A[6],B[6],Carry[6]);
-			fa FA3(Out_1[5],Carry[4],A[5],B[5],Carry[5]);
-			fa FA4(Out_1[4],Carry[3],A[4],B[4],Carry[4]);
-			fa FA5(Out_1[3],Carry[2],A[3],B[3],Carry[3]);
-			fa FA6(Out_1[2],Carry[1],A[2],B[2],Carry[2]);
-			fa FA7(Out_1[1],Carry[0],A[1],B[1],Carry[1]);
-		end
-	end
-	
-	
-	
-	
-endmodule
+output [8:0]out;
 
-module fa(s,co,a,b,ci);
-    output s,co;
-    input a,b,ci;
-    xor1 u1(s,a,b,ci);
-    and1 u2(n1,a,b);
-    and1 u3(n2,b,ci);
-    and1 u4(n3,a,ci);
-    or1 u5(co,n1,n2,n3);
+reg [8:0]out;
+
+wire in1,in2;
+//,flag;
+
+always@(s)
+
+begin
+
+case(s)
+
+4'b0000:           out=a+b;                 //8-bit addition
+
+4'b0001:           out=a-b;                  //8-bit subtraction
+
+4'b0010:           out=a*b;                 //8-bit multiplication
+
+4'b0011:           out=a/b;                  //8-bit division
+
+4'b0100:           out=in1%in2;                //8-bit modulo division
+
+4'b0101:           out=in1&&in2; //8-bit logical and
+
+4'b0110:           out=a||b;                  //8-bit logical or
+
+4'b0111:           out=!a;                      //8-bit logical negation
+
+4'b1000:           out=~a;                     //8-bit bitwise negation
+
+4'b1001:           out=a&b;                //8-bit bitwise and
+
+4'b1010:           out=a|b;                  //8-bit bitwise or
+
+4'b1011:           out=a^b;                 //8-bit bitwise xor
+
+4'b1100:           out=a<<1;                 //left shift
+
+4'b1101:           out=a>>1;                 //right shift
+
+4'b1110:           out=a+1;                   //increment
+
+4'b1111:           out=a-1;                    //decrement
+
+endcase
+
+end
+
+ 
+
 endmodule
